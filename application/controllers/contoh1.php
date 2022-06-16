b33671aa2b0b429b68e02995a66abcdbd5bff198
nvxg28 <rianhdyt529@gmail.com>
2022-06-16 16:04:31 +0700

    first commit

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+ public function index()
+ {
+ echo "<h1>Perkenalkan</h1>";
+ echo"Nama saya Rian Hidayat
+ Saya tinggal di Bintaro
+ olahraga yang saya sukai adalah
+ futsal";
+ }
+}
